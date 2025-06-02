8f54cbd8ac8914eb23a0a8bf9c162b8271644468
zzs <3522059578@qq.com>
2025-06-02 12:54:02 +0800

    搭建基本项目结构

---
+++ eayhelp/src/main/java/com/homework/easyhelp.java
@@ -0,0 +1,11 @@
+package com.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class easyhelp {
+    public static void main(String[] args) {
+        SpringApplication.run(easyhelp.class, args);
+    }
+}
