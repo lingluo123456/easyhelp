869bbf3b78dc735c89d8fb821ff945b77730d951
zzs <3522059578@qq.com>
2025-06-03 08:59:56 +0800

    加入注释

--- homework/src/main/java/com/easyhelp/assistant/Assistant.java
+++ homework/src/main/java/com/easyhelp/assistant/Assistant.java
@@ -6,11 +6,11 @@ import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService(
-        chatMemory = "chatMemory",
+        chatMemory = "chatMemory",// 配置记忆
         chatMemoryProvider = "chatMemoryProvider"
 )
 public interface Assistant {
-    @SystemMessage(fromResource = "easyhelp-prompt-template.txt")
+    @SystemMessage(fromResource = "easyhelp-prompt-template.txt")// 系统消息提示词
     String chat(@MemoryId Long memoryId,
                 @UserMessage String message);
 }
