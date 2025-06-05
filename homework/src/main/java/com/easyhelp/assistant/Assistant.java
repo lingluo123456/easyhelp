fd1ce8894174299d70b0740647b9f45dfe95bddb
Map1e <3412883097@qq.com>
2025-06-05 23:41:20 +0800

    完成流式输出

--- homework/src/main/java/com/easyhelp/assistant/Assistant.java
+++ homework/src/main/java/com/easyhelp/assistant/Assistant.java
@@ -4,15 +4,17 @@ import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
+import reactor.core.publisher.Flux;
 
 @AiService(
         chatMemory = "chatMemory",
+        streamingChatModel= "qwenStreamingChatModel",
         chatMemoryProvider = "chatMemoryProvider",
         tools = "appointmentTools",
         contentRetriever = "contentRetrieverEasyHelp"
 )
 public interface Assistant {
     @SystemMessage(fromResource = "EasyHelp-prompt-template.txt")
-    String chat(@MemoryId Long memoryId,
-                @UserMessage String message);
+    Flux<String> chat(@MemoryId Long memoryId,
+              @UserMessage String message);
 }
--- homework/src/main/java/com/easyhelp/controller/EasyHelpController.java
+++ homework/src/main/java/com/easyhelp/controller/EasyHelpController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @Tag(name = "EasyHelp")
 @RestController
@@ -18,7 +19,7 @@ public class EasyHelpController {
     private Assistant assistant;
     @Operation(summary = "对话")
     @PostMapping("/chat")
-    public String chat(@RequestBody ChatForm chatForm) {
+    public Flux<String> chat(@RequestBody ChatForm chatForm) {
         return assistant.chat(chatForm.getMemoryId(), chatForm.getMessage());
     }
 }
